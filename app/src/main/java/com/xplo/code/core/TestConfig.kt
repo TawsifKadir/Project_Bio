f71eeb8d864bdf8139975f879c7f8346e0079a0c
Moniruzzaman shadhin <shadhinemail@gmail.com>
2024-02-06 22:45:41 +0600

    Build update v5

--- app/src/main/java/com/xplo/code/core/TestConfig.kt
+++ app/src/main/java/com/xplo/code/core/TestConfig.kt
@@ -2,6 +2,6 @@ package com.xplo.code.core
 
 object TestConfig {
     val isValidationEnabled = true
-    val isDummyDataEnabled = true
-    val isLongClickDGEnabled = true
+    val isDummyDataEnabled = false
+    val isLongClickDGEnabled = false
 }
