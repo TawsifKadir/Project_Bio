de6cff30fe1fdf8bae554e7ee983cb0421bbb5a8
Nasif <nasif.002@gmail.com>
2024-02-09 14:28:16 +0600

    add more property in test config

--- app/src/main/java/com/xplo/code/core/TestConfig.kt
+++ app/src/main/java/com/xplo/code/core/TestConfig.kt
@@ -1,8 +1,12 @@
 package com.xplo.code.core
 
 object TestConfig {
-    val isValidationEnabled = true
-    val isDummyDataEnabled = true
+
+    val isValidationEnabled = true      // default true
+
+    val isDummyDataEnabled = true       // default false
     val isLongClickDGEnabled = true
-    val isNomineeAlwaysEnabled = true
+    val isAutoDGEnabled = true
+
+    val isNomineeAlwaysEnabled = true   // default false
 }
