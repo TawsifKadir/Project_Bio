3740df7d42c37d9f167ff28c318f0bc7d0ce6c1b
Moniruzzaman shadhin <shadhinemail@gmail.com>
2024-03-05 01:40:22 +0600

    beneficiary data show

--- app/src/main/java/com/xplo/code/ui/dashboard/household/forms/FormDetailsFragment.kt
+++ app/src/main/java/com/xplo/code/ui/dashboard/household/forms/FormDetailsFragment.kt
@@ -12,7 +12,6 @@ import com.xplo.code.core.Bk
 import com.xplo.code.core.ext.loadAvatar
 import com.xplo.code.data.db.models.HouseholdItem
 import com.xplo.code.data.db.models.toHouseholdForm
-import com.xplo.code.data.db.room.model.Beneficiary
 import com.xplo.code.databinding.FragmentFormDetailsBinding
 import com.xplo.code.ui.components.ReportViewUtils
 import com.xplo.code.ui.dashboard.household.HouseholdContract
@@ -62,6 +61,7 @@ class FormDetailsFragment : BaseFragment(), HouseholdContract.FormDetailsView {
            // Log.d(TAG, "newInstance() called with: parent = $parent, item = ${item?.hid}")
             val fragment = FormDetailsFragment()
             fragment.item = item?.applicationId
+            fragment.beneficiary = item
             //val bundle = Bundle()
             //bundle.putString(Bk.KEY_PARENT, parent)
             //bundle.putSerializable(Bk.KEY_ITEM, item)
@@ -78,6 +78,7 @@ class FormDetailsFragment : BaseFragment(), HouseholdContract.FormDetailsView {
 
     private var householdItem : HouseholdItem? = null
     private var item : String? = null
+    private var beneficiary : com.kit.integrationmanager.model.Beneficiary? = null
 
 
     override fun onCreateView(
@@ -104,11 +105,11 @@ class FormDetailsFragment : BaseFragment(), HouseholdContract.FormDetailsView {
     }
 
     override fun initView() {
-
-        if (arguments != null) {
-            householdItem = arguments?.getSerializable(Bk.KEY_ITEM) as HouseholdItem
-            onGetCompleteData(householdItem)
-        }
+        generateReportFromBeneficiary(beneficiary)
+//        if (arguments != null) {
+//            householdItem = arguments?.getSerializable(Bk.KEY_ITEM) as HouseholdItem
+//            onGetCompleteData(householdItem)
+//        }
     }
 
     override fun initObserver() {
@@ -154,6 +155,84 @@ class FormDetailsFragment : BaseFragment(), HouseholdContract.FormDetailsView {
         addReportForm5(form.form5)
         addReportForm6(form.form6)
         addReportAlternate(form)
+    }
+    private fun generateReportFromBeneficiary(beneficiary: com.kit.integrationmanager.model.Beneficiary?) {
+        //Log.d(TAG, "generateReport() called with: form = $form")
+        if (beneficiary == null) return
+
+        val form1 = HhForm1()
+        form1.lon = beneficiary.location.lon
+        form1.lat = beneficiary.location.lat
+        form1.state?.name = beneficiary.address.stateId.toString()
+        form1.payam?.name =  beneficiary.address.payam.toString()
+        form1.boma?.name =  beneficiary.address.boma.toString()
+        form1.county?.name =  beneficiary.address.countyId.toString()
+        addReportForm1(form1)
+
+        val form2 = HhForm2()
+        form2.firstName = beneficiary.respondentFirstName
+        form2.middleName = beneficiary.respondentMiddleName
+        form2.lastName =  beneficiary.respondentLastName
+        form2.nickName = beneficiary.respondentNickName
+        form2.spouseFirstName = beneficiary.spouseFirstName
+        form2.spouseMiddleName = beneficiary.spouseMiddleName
+        form2.spouseLastName = beneficiary.spouseLastName
+        form2.spouseNickName = beneficiary.respondentNickName
+        form2.age = beneficiary.respondentAge
+        form2.idNumber = beneficiary.respondentId
+
+        if(beneficiary.documentType != null){
+            form2.idNumberType = beneficiary.documentType.value
+        }
+        form2.phoneNumber = beneficiary.respondentPhoneNo
+
+        if (beneficiary.householdIncomeSource != null){
+            form2.mainSourceOfIncome = beneficiary.householdIncomeSource.value
+        }
+        if(beneficiary.householdIncomeSource != null){
+            form2.currency = beneficiary.householdIncomeSource.value
+        }
+        form2.monthlyAverageIncome = beneficiary.householdMonthlyAvgIncome
+
+        if(beneficiary.respondentGender != null){
+            form2.gender = beneficiary.respondentGender.value
+        }
+
+        if(beneficiary.respondentMaritalStatus != null){
+            form2.respondentRlt = beneficiary.respondentMaritalStatus.value
+        }
+        if(beneficiary.respondentMaritalStatus != null){
+            form2.maritalStatus = beneficiary.respondentMaritalStatus.value
+        }
+        if(beneficiary.respondentLegalStatus != null){
+            form2.legalStatus = beneficiary.respondentLegalStatus.value
+        }
+        if(beneficiary.selectionReason[0] != null){
+            form2.selectionReason = beneficiary.selectionReason[0].value
+        }
+        if(beneficiary.selectionCriteria != null){
+            form2.selectionCriteria = beneficiary.selectionCriteria.value
+        }
+        addReportForm2(form2)
+
+        /*
+        val form3 = HhForm3()
+        form3.female0_2 = beneficiary.householdMember2.totalFemale
+        addReportForm3(form.form3)
+
+        val form4 = HhForm4()
+        form4.photoData = beneficiary.biometrics[0].biometricData[0].pho
+
+        val form5 = HhForm5()
+        form5.fingers = beneficiary.biometrics
+        addReportForm5(form5)
+
+        val form6 = HhForm6()
+        form6.nominees = beneficiary.nominees
+        addReportForm6(form6)
+
+        addReportAlternate(form)
+         */
     }
 
     private fun addReportForm1(form: HhForm1?) {
