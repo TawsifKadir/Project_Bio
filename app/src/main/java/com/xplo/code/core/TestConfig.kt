4012a7aba3e75fbc92ab54afebb81f6664eefd41
Moniruzzaman shadhin <shadhinemail@gmail.com>
2024-03-16 22:44:57 +0600

    build version updated

--- app/src/main/java/com/xplo/code/core/TestConfig.kt
+++ app/src/main/java/com/xplo/code/core/TestConfig.kt
@@ -2,8 +2,16 @@ package com.xplo.code.core
 
 object TestConfig {
 
+    //for build just make it false
+    val isDummyDataEnabled = false  // default false
+
+
+
+
+
+
+
     val isValidationEnabled = true      // default true
-    val isDummyDataEnabled = true                                        // default false
     val isLongClickDGEnabled = true
     val isAutoDGEnabled = true
 
@@ -12,15 +20,12 @@ object TestConfig {
     val isNomineeAlwaysEnabled = true           // default true
     val isAlternateAddInHouseholdFlow = true    // default true
 
-    val isFakeLoginEnabled = false       // default true
-    //val isTestLoginEnabled = false    // default false
+    val isFakeLoginEnabled = false       // default false
 
     // only for nasif, for a temp purpose, will removed later
     val isNavHackEnabled = false        // default false
     val isNomineeFlow2Enabled = true    // default true
-    val isDevBarEnabled = true         // default false
 
     val isFingerPrintRequired = true   // default true
-    val isAlternateRequired = false     // default true
 
 }
